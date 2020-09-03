bdbe3f6ebd64b5433bfa12b363a47fedb452edfc
Vyacheslav <kosv2007@yandex.ru>
2020-09-03 21:48:30 +0500

    added brain-calc.js and index.js

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+
+export const commonLogic = (question, f()) => {
+  let equal = true;
+  let countQuestions = 3;
+
+  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log(`Hello, ${name}!\n${question}`);
+
+  while (equal && (countQuestions > 0)) {
+    const [desiredAnswer, text] = f();
+    const answer = readlineSync.question(`Question: ${text}\nYour answer: `);
+    if (answer === desiredAnswer.toString()) {
+      console.log('Correct');
+      countQuestions -= 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${desiredAnswer}".\nLet's try again, ${name}!`);
+      equal = false;
+    }
+  }
+
+  if (countQuestions === 0) {
+    console.log(`Congratulations, ${name}!`);
+  }
+};
